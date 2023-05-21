a07a8da87b5721d8c6bdbe0e2f24341762bf418b
Kriosaber <jorge.quinones@uniat.edu.mx>
2023-05-21 16:26:13 -0700

    Funcionalidad del menú creada
    El navbar ya desplaza el scroll hacia las secciones y ya se queda fija en la pantalla, coloqué texto de relleno para probar, y utilice un poco de js para calcular donde queda el scroll para que no haga overlap al contenido

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 const toggleMenuElement = document.getElementById('toggle-button');
 const mainMenuElement = document.getElementById('menu-list');
+const navigationHeight = document.getElementById('nav-bar').offsetHeight;
 
 toggleMenuElement.addEventListener('click', ()=>{
     mainMenuElement.classList.toggle('menu-list--show');
 });
+
+document.documentElement.style.setProperty('--scroll-padding', navigationHeight + 1 + "px");
+
