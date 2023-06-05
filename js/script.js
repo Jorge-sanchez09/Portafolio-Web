57de4ce5f497654c1ec712b019ba6339f8c598aa
Kriosaber <jorge.quinones@uniat.edu.mx>
2023-06-04 21:16:05 -0700

    Sección de proyectos - placeholder
    Comencé la sección de proyectos, para lo cual hice un placeholder con la misma imagen para cada proyecto y textos lorem ipsum para cada proyecto y así solo tengo que reemplazar las imagenes y los textos

--- js/script.js
+++ js/script.js
@@ -1,9 +1,23 @@
 const toggleMenuElement = document.getElementById('toggle-button');
 const mainMenuElement = document.getElementById('menu-list');
 const navigationHeight = document.getElementById('nav-bar').offsetHeight;
+const projects = document.querySelectorAll('.project');
 
 toggleMenuElement.addEventListener('click', ()=>{
     mainMenuElement.classList.toggle('menu-list--show');
 });
 
 document.documentElement.style.setProperty('--scroll-padding', navigationHeight + 1 + "px");
+
+for(const project of projects){
+    project.addEventListener('click', (e) => 
+    {
+        if(e.target.parentElement.classList.contains("project__header"))
+        {
+            const projectContent = project.querySelector('.project__content');
+            projectContent.classList.toggle("project__content--show");
+
+            project.querySelector(".project__title").classList.toggle("project__title--fixed");
+        }
+    });
+}
