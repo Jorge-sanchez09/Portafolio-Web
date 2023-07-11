0b9cff7f2f8a64f8592f4ff8c4e0e71e75f2886c
Kriosaber <jorge.quinones@uniat.edu.mx>
2023-07-10 18:45:40 -0700

    Sección de contacto creada
    El HTML y el CSS de la sección de contacto está listo, falta darle funcionalidad

--- js/script.js
+++ js/script.js
@@ -4,12 +4,27 @@ const navigationHeight = document.getElementById('nav-bar').offsetHeight;
 const projects = document.querySelectorAll('.project');
 const circularProgress = document.querySelectorAll('.skills__circular-bar');
 
+// nuevo
+const projectsDiv = document.getElementById('projects-container');
+
 toggleMenuElement.addEventListener('click', ()=>{
     mainMenuElement.classList.toggle('menu-list--show');
 });
 
 document.documentElement.style.setProperty('--scroll-padding', navigationHeight + "px");
 
+projectsDiv.addEventListener('click', (e) => {
+    if(e.target.parentElement.classList.contains("project__header"))
+    {
+       const projectContent = e.target.parentElement.parentElement.querySelector('.project__content');
+       projectContent.classList.toggle("project__content--show");
+    
+       const projectTitle = e.target.parentElement.querySelector('.project__title');
+       projectTitle.classList.toggle("project__title--fixed");
+    }   
+});
+
+/*
 for(const project of projects){
     project.addEventListener('click', (e) => 
     {
@@ -22,6 +37,7 @@ for(const project of projects){
         }
     });
 }
+*/
 
 Array.from(circularProgress).forEach(circular => {
     const circularProgressValue = getComputedStyle(circular)
