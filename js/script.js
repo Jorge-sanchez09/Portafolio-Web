7f73ed5a276b942a59e600e0fd2689196821af39
Kriosaber <jorge.quinones@uniat.edu.mx>
2023-07-14 14:34:54 -0700

    Animaciones en la pagina al hacer scroll
    Le agregué algunas transiciones a varios elementos de la pagina, las cuales se ejecutan al estar el elemento dentro del viewport, por lo que se mostraran las transiciones al hacer scroll y se vea el elemento, también agregué algunos links a mis redes en Sobre mi y otros cambios menores

--- js/script.js
+++ js/script.js
@@ -1,17 +1,23 @@
+// Menú de navegación
 const toggleMenuElement = document.getElementById('toggle-button');
 const mainMenuElement = document.getElementById('menu-list');
 const navigationHeight = document.getElementById('nav-bar').offsetHeight;
+
+// Contenedor de los proyectos
+const projectsDiv = document.getElementById('projects-container');
 const projects = document.querySelectorAll('.project');
+
+// Barras de skills
+const skillsBars = document.querySelectorAll('.skills__bar-fill')
+
+// Barras circulares 
 const circularProgress = document.querySelectorAll('.skills__circular-bar');
 
-// nuevo
-const projectsDiv = document.getElementById('projects-container');
+document.documentElement.style.setProperty('--scroll-padding', navigationHeight + "px");
 
 toggleMenuElement.addEventListener('click', ()=>{
     mainMenuElement.classList.toggle('menu-list--show');
 });
-
-document.documentElement.style.setProperty('--scroll-padding', navigationHeight + "px");
 
 projectsDiv.addEventListener('click', (e) => {
     if(e.target.parentElement.classList.contains("project__header"))
@@ -24,37 +30,67 @@ projectsDiv.addEventListener('click', (e) => {
     }   
 });
 
-/*
-for(const project of projects){
-    project.addEventListener('click', (e) => 
-    {
-        if(e.target.parentElement.classList.contains("project__header"))
-        {
-            const projectContent = project.querySelector('.project__content');
-            projectContent.classList.toggle("project__content--show");
+// Animaciones de las barras circulares
+let filled = false;
+function circularBarAnimation(){
+    if(!filled){
+        circularProgress.forEach(bar => {
+            const circularProgressValue = getComputedStyle(bar).getPropertyValue('--skill-bar-length').replace('%', ''); 
+    
+            let circularBarStartValue = 0, 
+            circularBarSpeed = 30;
+        
+            let circularBarProgress = setInterval(() => {
+                circularBarStartValue++;
+        
+                bar.style.background = ` conic-gradient(var(--clr-primary) ${circularBarStartValue}%, black 0deg)`;
+        
+                if(circularBarStartValue >= circularProgressValue){
+                    clearInterval(circularBarProgress);
+                    filled = true;
+                }
+        
+            }, circularBarSpeed);
+        })    
+    }     
+}
 
-            project.querySelector(".project__title").classList.toggle("project__title--fixed");
+// IntersectionObserver para las animaciones de los elementos cuando están visibles en el viewport
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if(entry.isIntersecting){
+            const element = entry.target;
+            if(element.classList.contains('fade--left'))
+                element.classList.add('fade--left-show');
+            else if(element.classList.contains('fade--right'))
+                element.classList.add('fade--right-show');
+            else if(element.classList.contains('skills__bar-fill'))
+                element.classList.add('skills__bar-fill--animate');
+            else if(element.classList.contains('skills__circular-bar'))
+                circularBarAnimation(element);
         }
     });
-}
-*/
-
-Array.from(circularProgress).forEach(circular => {
-    const circularProgressValue = getComputedStyle(circular)
-    .getPropertyValue('--skill-bar-length').replace('%', ''); 
-
-    let circularBarStartValue = 0, 
-    circularBarSpeed = 30;
-
-    let circularBarProgress = setInterval(() => {
-        circularBarStartValue++;
-
-        circular.style.background = ` conic-gradient(var(--clr-primary) ${circularBarStartValue}%, black 0deg)`;
-
-        if(circularBarStartValue >= circularProgressValue){
-            clearInterval(circularBarProgress);
-        }
-
-    }, circularBarSpeed);
 });
 
+// Otro observer a parte para animar los proyectos, ya que este es un poco diferente
+const projectsObserver = new IntersectionObserver(entries => {
+    let transitionDelay = 0;
+    entries.forEach(entry => {
+        const element = entry.target;
+        if(entry.isIntersecting){
+            element.style.setProperty('--transition-delay', transitionDelay + 's');
+            element.classList.add('project--show');
+            transitionDelay += 0.1;
+        }
+    })
+});
+
+const fadeLeftElements = document.querySelectorAll('.fade--left');
+const fadeRightElements = document.querySelectorAll('.fade--right');
+
+// Animaciones cuando los elementos estén dentro del viewport
+fadeLeftElements.forEach(element => observer.observe(element));
+fadeRightElements.forEach(element => observer.observe(element));
+projects.forEach(element => projectsObserver.observe(element));
+skillsBars.forEach(element => observer.observe(element));
+circularProgress.forEach(element => observer.observe(element));
